3f7c9ac38d277fe025412783a74c6afb152de499
Lorenz Brun <lorenz@brun.one>
2021-08-17 01:00:18 +0200

    Raw ioctl handling improvements

--- ioctl/ioctl.go
+++ ioctl/ioctl.go
@@ -3,6 +3,7 @@
 package ioctl
 
 import (
+	"errors"
 	"runtime"
 	"unsafe"
 
@@ -19,36 +20,48 @@ func NvlistIoctl(fd uintptr, ioctl Ioctl, name string, cmd *Cmd, request interfa
 		if src, err = nvlist.Marshal(request); err != nil {
 			return err
 		}
-		//ioutil.WriteFile("ioctl-req.bin", src, 0644)
 	}
-	// WARNING: Here be dragons! This is completely outside of Go's safety net and uses various
-	// criticial runtime workarounds to make sure that memory is safely handled
-	dst := make([]byte, 32*1024)
-	if response != nil {
-		cmd.Nvlist_dst = uint64(uintptr(unsafe.Pointer(&dst[0])))
-		cmd.Nvlist_dst_size = uint64(len(dst))
-	}
-	if request != nil {
-		cmd.Nvlist_src = uint64(uintptr(unsafe.Pointer(&src[0])))
-		cmd.Nvlist_src_size = uint64(len(src))
-	}
-	if config != nil {
-		if configRaw, err = nvlist.Marshal(config); err != nil {
-			return err
+	dst := make([]byte, 8*1024)
+	for {
+		// This is necessary as some ioctl handlers modify the command buffer even though they
+		// later return ENOMEM and we retry the call.
+		privateCmd := *cmd
+		// WARNING: Here be dragons! This is completely outside of Go's safety net and uses various
+		// criticial runtime workarounds to make sure that memory is safely handled
+		if response != nil {
+			privateCmd.Nvlist_dst = uint64(uintptr(unsafe.Pointer(&dst[0])))
+			privateCmd.Nvlist_dst_size = uint64(len(dst))
 		}
-		cmd.Nvlist_conf = uint64(uintptr(unsafe.Pointer(&configRaw[0])))
-		cmd.Nvlist_conf_size = uint64(len(configRaw))
+		if request != nil {
+			privateCmd.Nvlist_src = uint64(uintptr(unsafe.Pointer(&src[0])))
+			privateCmd.Nvlist_src_size = uint64(len(src))
+		}
+		if config != nil {
+			if configRaw, err = nvlist.Marshal(config); err != nil {
+				return err
+			}
+			privateCmd.Nvlist_conf = uint64(uintptr(unsafe.Pointer(&configRaw[0])))
+			privateCmd.Nvlist_conf_size = uint64(len(configRaw))
+		}
+		stringToDelimitedBuf(name, privateCmd.Name[:])
+		_, _, errno := unix.Syscall(unix.SYS_IOCTL, fd, uintptr(ioctl), uintptr(unsafe.Pointer(&privateCmd)))
+		runtime.KeepAlive(src)
+		runtime.KeepAlive(dst)
+		runtime.KeepAlive(privateCmd)
+		runtime.KeepAlive(configRaw)
+		if errno == unix.ENOMEM {
+			if len(dst) >= 16*1024*1024 {
+				return errors.New("return buffer is bigger than 16MiB, something probably went wrong")
+			}
+			dst = make([]byte, len(dst)*8)
+			continue
+		}
+		*cmd = privateCmd
+		if errno != 0 {
+			return errno
+		}
+		break
 	}
-	stringToDelimitedBuf(name, cmd.Name[:])
-	_, _, errno := unix.Syscall(unix.SYS_IOCTL, fd, uintptr(ioctl), uintptr(unsafe.Pointer(cmd)))
-	runtime.KeepAlive(src)
-	runtime.KeepAlive(dst)
-	runtime.KeepAlive(cmd)
-	runtime.KeepAlive(configRaw)
-	if errno != 0 {
-		return errno
-	}
-	//ioutil.WriteFile("ioctl-res.bin", dst, 0644)
 	if response != nil {
 		return nvlist.Unmarshal(dst, response)
 	}
